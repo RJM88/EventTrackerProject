0232011963a7d2bf40bb08de582773c8834d1a26
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-11-13 16:11:10 -0700

    JUnit passes

--- JPAWorkout/src/main/java/com/skilldistillery/workout/entities/Workout/Workout.java
+++ JPAWorkout/src/main/java/com/skilldistillery/workout/entities/Workout/Workout.java
@@ -1,5 +1,71 @@
 package com.skilldistillery.workout.entities.Workout;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Workout {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+
+	public Workout() {
+		super();
+	}
+
+	public Workout(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Workout other = (Workout) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Workout [id=").append(id).append(", name=").append(name).append("]");
+		return builder.toString();
+	}
 
 }
--- JPAWorkout/src/test/java/com/skilldistillery/workout/entities/Workout/WorkoutTest.java
+++ JPAWorkout/src/test/java/com/skilldistillery/workout/entities/Workout/WorkoutTest.java
@@ -2,6 +2,10 @@ package com.skilldistillery.workout.entities.Workout;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,26 +13,36 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class WorkoutTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Workout workout;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("WorkoutPU");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		workout = em.find(Workout.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		workout = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_Workout_Mapping() {
+		assertNotNull(workout);
+		assertEquals("Monday", workout.getName());
 	}
 
 }
